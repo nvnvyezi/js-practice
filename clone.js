3b0a33f462c8c80f07012b32d47bf749e36117e4
nvnvyezi <1187128658@qq.com>
2019-03-17 11:14:26 +0800

    fix clone

--- clone.js
+++ clone.js
@@ -1,13 +1,11 @@
 var obj = {
   name : 'miaorenjie',
   age: 20,
-  // like: ['black', 'white'],
+  like: ['black', 'white'],
 };
 obj.c = obj
 
-console.log(obj)
-
-// // 浅拷贝
+// 浅拷贝
 function shallowCopy (arr) {
   let res = {};
   for (const key in arr) {
@@ -24,11 +22,10 @@ const obj1 = shallowCopy(obj);
 // 深拷贝
 function deepCopy (obj) {
   let arr;
-  if (typeof obj === 'object') {
-    arr = obj.constructor === Array ? [] : {};
-  } else {
-    return obj;
+  if (typeof obj !== 'object') {
+    return obj
   }
+  arr = Array.isArray(obj) ? [] : {};
   for (const key in obj) {
     if (obj.hasOwnProperty(key)) {
       arr[key] = deepCopy(obj[key]);
@@ -37,78 +34,27 @@ function deepCopy (obj) {
   return arr;
 }
 
-const obj2 = deepCopy(obj)
-console.log(obj2, obj)
+// const obj2 = deepCopy(obj)
+// console.log(obj2, obj)
 
 // 循环引用
-
-function deepCopy (obj, parent) {
-  let res = {};
-  let _parent = parent;
-  while (_parent) {
-    if (_parent.origin === obj) {
-      return _parent.currentParent;
-    }
-    _parent = _parent.parent;
+const memory = new Map()
+function deepCopy2(param) {
+  if (typeof param !== 'object') {
+    return param
   }
-  for (const key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      if (typeof obj[key] === 'object') {
-        res[key] = deepCopy(obj[key], {origin: obj, currentParent: res, parent: parent});
-      } else {
-        res[key] = obj[key];
-      }
+  const arr = Array.isArray(param) ? [] : {}
+  const res = memory.get(param)
+  if (res) {
+    return res
+  }
+  memory.set(param, param)
+  for (const key in param) {
+    if (param.hasOwnProperty(key)) {
+      const item = param[key];
+      arr[key] = deepCopy2(item)
     }
   }
-  return res;
+  return arr
 }
-
-console.log(deepCopy(obj))
-
-var a = {
-  a1: 1,
-  a2: {
-      b1: 1,
-      b2: {
-          c1: 1
-      }
-  },
-  a3: [1, 2, 3]
-}
-// a.a3 = a;
-
-let count = new Map();
-
-function deep(obj) {
-  let o = Array.isArray(obj) ? [] : {};
-  const arr = [{
-    parent: o,
-    key: undefined,
-    data: obj
-  }]
-  while (arr.length) {
-    const node = arr.shift();
-    const {parent, key, data} = node;
-    let res = parent;
-    if (typeof key !== 'undefined') {
-      res = parent[key] = {};
-    }
-    for (const key in data) {
-      if (data.hasOwnProperty(key)) {
-        const element = data[key];
-        if (typeof element === 'object') {
-          arr.push({
-            parent: res,
-            key,
-            data: element
-          })
-        } else {
-          res[key] = element;
-        }
-      }
-    }
-  }
-  return o;
-}
-
-console.log(deep(a))
+console.log(deepCopy2(obj))
