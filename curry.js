03c307a1d727cb0010c58b3d2fc3b8b2e96d8526
nvnvyezi <nvnvyezi@gmail.com>
2019-05-16 20:27:43 +0800

    fix 柯里化

--- curry.js
+++ curry.js
@@ -3,51 +3,15 @@
  * 函数柯里化原理：将含有多个参数的函数转换成一系列使用一个参数的函数
  */
 
-//  第四版，不用知道求值参数个数,返回貌似有点小问题
-function curry (args) {
-  let arr = [];
-  function inlay(num) {
-    arr.push(num);
-    const res = arr.reduce((init, item) => {
-      return init * item;
+function curry(param) {
+  const args = [];
+  function inlay(...params) {
+    args.push(...params);
+    const num = args.reduce((pre, cur) => {
+      return pre * cur;
     }, 1);
-    inlay.res = () => console.log(res);
+    inlay.num = num;
     return inlay;
   }
-  return inlay(args);
+  return inlay(param);
 }
-
-// curry(2)(3).res();
-// curry(2)(3)(4).res();
-// curry(2)(3)(4)(5).res();
-
-function addCurry () {
-  let args = [];
-  function infn () {
-    args = [...args, ...arguments];
-    let sum = args.reduce((init, item) => {
-      return init + item;
-    }, 0);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-    infn.res = () => console.log(sum)
-    infn.toString = () => console.log(sum)
-    return infn;
-  }
-  return infn(...arguments);
-}
-
-function curr() {
-  let args = [...arguments];
-  return (function b() {
-    args = [...args, ...arguments];
-    b.c = args.reduce((init, item) => {
-      return init + item;
-    }, 0)
-    return b;
-  })
-}
-let a = curr(2)(3)(4)(5);
-console.log(a.c)
-// curry(2, 3, 4)(5).res();
-// addCurry(2, 3, 4)(5).res();
-// addCurry(1)(2)(3)(4)(5).res();
-// console.log(addCurry(1)(2)(3)(4)(5))
