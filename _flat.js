185b279b26161ec61f87fd291114e5da7d8849fc
nvnvyezi <nvnvyezi@gmail.com>
2019-07-30 22:39:50 +0800

    fix flatten

--- _flat.js
+++ _flat.js
@@ -20,6 +20,14 @@ Array.prototype._flat = function(level = 1) {
   return res;
 };
 
+Array.prototype._flatten = function() {
+  let res = this;
+  while (res.some(item => Array.isArray(item))) {
+    res = [].concat(...res);
+  }
+  return res;
+};
+
 /**
  * 已知如下数组：
  *
@@ -28,4 +36,4 @@ Array.prototype._flat = function(level = 1) {
  * 编写一个程序将数组扁平化去并除其中重复部分数据，最终得到一个升序且不重复的数组
  */
 var arr = [[1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14]]]], 10];
-const res = [...new Set(arr._flat(Infinity))].sort((a, b) => a - b);
+const res = [...new Set(arr._flatten(Infinity))].sort((a, b) => a - b);
